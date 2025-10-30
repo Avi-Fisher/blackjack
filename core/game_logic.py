6d271d304030238bef9cfdc18f67549226aedebe
Avi Fisher <avifisher82@gmail.com>
2025-10-30 12:09:10 +0200

    FINISH PROJJJJJJJJJJJECT

--- core/game_logic.py
+++ core/game_logic.py
@@ -38,9 +38,6 @@ def dealer_play(deck: list[dict], dealer: dict) -> bool:
     elif value <= 21 and value >= 17:
         return True
 
-
-
-
 def run_full_game(deck: list[dict], player: dict, dealer: dict) -> None:
     deal_two_each(deck,player,dealer)
 
@@ -60,18 +57,22 @@ def run_full_game(deck: list[dict], player: dict, dealer: dict) -> None:
                 exit("goodbye")
 
         elif user_play == "S":
-            dealer_value = dealer_play(deck,dealer)
+            if dealer_value > value_plyer:
+                print("you lose")
+                exit("goodbye")
 
-            if not dealer_value:
+            elif not dealer_value:
                 print("you winnn !!!!")
                 exit("goodbye")
 
             elif dealer_value < value_plyer:
                 print("you winnn !!!!!")
                 exit("goodbye")
-            elif dealer_value < value_plyer:
+
+            elif dealer_value > value_plyer:
                 print("you loseeeee")
                 exit("delear take all your muney")
+
             elif value_plyer == dealer_value:
                 print("tako no one win")
                 exit("goodbye")
