ade4d169dfad10f61f6c79c89113854fbae21d9d
Avi Fisher <avifisher82@gmail.com>
2025-10-30 10:44:29 +0200

    creat a "deal_two_each" function

--- core/game_logic.py
+++ core/game_logic.py
@@ -13,15 +13,25 @@ def calculate_hand_value(hand: list[dict]) -> int:
 
     return value
 
+def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
+
+    for i in range(2):
+        value = calculate_hand_value(deck)
+        card = deck.pop(0)
+        player.append(card)
+
+    for i in range(2):
+        value = calculate_hand_value(deck)
+        card = deck.pop(0)
+        dealer.append(card)
 
 
 
 
 
 
-#
-# def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
-#
+
+
 # def dealer_play(deck: list[dict], dealer: dict) -> bool:
 #
 # def run_full_game(deck: list[dict], player: dict, dealer: dict) -> None:
