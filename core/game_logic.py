cd9c765455d68694a089d543896933e2ecb07707
Avi Fisher <avifisher82@gmail.com>
2025-10-30 09:35:48 +0200

    creat a "build_standard_deck" function

---
+++ core/game_logic.py
@@ -0,0 +1,34 @@
+def calculate_hand_value(hand: list[dict]) -> int:
+
+def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
+
+def dealer_play(deck: list[dict], dealer: dict) -> bool:
+
+def run_full_game(deck: list[dict], player: dict, dealer: dict) -> None:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
