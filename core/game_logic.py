a024affae020183fd5d78ef48f493bfdcee3376d
Avi Fisher <avifisher82@gmail.com>
2025-10-30 12:02:18 +0200

    FINISH PROJJJJJJJJJJJECT

--- core/game_logic.py
+++ core/game_logic.py
@@ -1,37 +1,79 @@
-def calculate_hand_value(hand: list[dict]) -> int:
+from core.player_io import ask_player_action
 
-    card = hand[0]["rank"]
+
+def calculate_hand_value(hand: list[dict]) -> int:
     value = 0
 
-    try:
-        value = int(card)
-    except:
-        if card == "A":
-            value = 1
-        else:
-            value = 10
+    for i in hand["hand"]:
+        try:
+            value += int(i["rank"])
+        except:
+            if i["rank"] == "A":
+                value += 1
+            else:
+                value += 10
 
     return value
 
 def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
 
     for i in range(2):
-        value = calculate_hand_value(deck)
-        card = deck.pop(0)
-        player.append(card)
+        player["hand"].append(deck.pop(0))
+    print(f"your value is {calculate_hand_value(player)}")
 
     for i in range(2):
-        value = calculate_hand_value(deck)
-        card = deck.pop(0)
-        dealer.append(card)
+        dealer["hand"].append(deck.pop(0))
+    print(f"dealer value is {calculate_hand_value(dealer)}")
+
+def dealer_play(deck: list[dict], dealer: dict) -> bool:
+    value = 0
+
+    while value < 17:
+        dealer["hand"].append(deck.pop(0))
+        value = calculate_hand_value(dealer)
+        print(f"value dealer is {value}")
+
+    if value > 21:
+        return False
+    elif value <= 21 and value >= 17:
+        return True
 
 
 
 
+def run_full_game(deck: list[dict], player: dict, dealer: dict) -> None:
+    deal_two_each(deck,player,dealer)
+
+    value_plyer = calculate_hand_value(player)
+    dealer_value = calculate_hand_value(dealer)
+
+    while True:
+        user_play = ask_player_action()
+
+        if user_play == "H":
+            player["hand"].append(deck.pop(0))
+            value_plyer = calculate_hand_value(player)
+            print(f"your vlaue is {value_plyer}")
+
+            if value_plyer > 21:
+                print("you lose")
+                exit("goodbye")
+
+        elif user_play == "S":
+            dealer_value = dealer_play(deck,dealer)
+
+            if not dealer_value:
+                print("you winnn !!!!")
+                exit("goodbye")
+
+            elif dealer_value < value_plyer:
+                print("you winnn !!!!!")
+                exit("goodbye")
+            elif dealer_value < value_plyer:
+                print("you loseeeee")
+                exit("delear take all your muney")
+            elif value_plyer == dealer_value:
+                print("tako no one win")
+                exit("goodbye")
 
 
-
-
-# def dealer_play(deck: list[dict], dealer: dict) -> bool:
-#
-# def run_full_game(deck: list[dict], player: dict, dealer: dict) -> None:
---
+++ core/main.py
@@ -0,0 +1,29 @@
+from core.back import build_standard_deck, shuffle_by_suit
+from core.game_logic import calculate_hand_value, deal_two_each, run_full_game
+
+if __name__ == "__main__":
+
+    deck = build_standard_deck()
+    deck = shuffle_by_suit(deck)
+
+    player = {"hand":[]}
+    dealer = {"hand":[]}
+
+    run_full_game(deck,player,dealer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
